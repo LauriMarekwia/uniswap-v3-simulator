820ba57c186550715a7e7d3122a916465f06f27a
LauriMarekwia <lauri@tesseractinvestment.com>
2023-08-16 09:32:32 +0300

    amended block

--- src/manager/EventDBManager.ts
+++ src/manager/EventDBManager.ts
@@ -166,11 +166,11 @@ export class EventDBManager {
   }
 
   getInitializationEventBlockNumber(): Promise<number> {
-    return Promise.resolve(16331949);
+    return Promise.resolve(16343806);
 }
 
   getLatestEventBlockNumber(): Promise<number> {
-    return Promise.resolve(16331949);
+    return Promise.resolve(16343806);
 }
 
 
@@ -310,7 +310,7 @@ export class EventDBManager {
   saveInitializationEventBlockNumber(
     initializationEventBlockNumber: number
   ): Promise<number> {
-    initializationEventBlockNumber = 16331949;
+    initializationEventBlockNumber = 16343806;
     return this.knex.transaction((trx) =>
       this.updateInitializationEventBlockNumber(
         initializationEventBlockNumber,
@@ -513,7 +513,7 @@ export class EventDBManager {
       amount1: JSBIDeserializer(event.amount1),
       tickLower: event.tick_lower,
       tickUpper: event.tick_upper,
-      blockNumber: 16331949,
+      blockNumber: 16343806,
       transactionIndex: event.transaction_index,
       logIndex: event.log_index,
       date: DateConverter.parseDate(event.date),
